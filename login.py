c6e81c0022684f7a5eb87ba037a3be5e3295c91b
eolv <eolv@vip.qq.com>
2021-10-13 00:34:32 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,17 +1,10 @@
-import time
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
- 
-def login_mail(username, password):
-	opts = FirefoxOptions()
-	opts.add_argument('–headless')
-	browser = webdriver.Firefox(firefox_options=opts)
-	browser.visit('https://mail.pku.edu.cn/')
-	browser.find_by_id('username').fill(username)
-	browser.find_by_id('password').fill(password)
-	browser.find_by_id('login_button').click()
-	time.sleep(5)
-	browser.quit()
-	
-if __name__ == '__main__':
-	login_mail('2101210560', 'dzn-0419')
+
+firefox_options = webdriver.FirefoxOptions()
+driver = webdriver.Remote(
+    command_executor='http://www.example.com',
+    options=firefox_options
+)
+driver.get("http://www.google.com")
+driver.quit() 
+  
