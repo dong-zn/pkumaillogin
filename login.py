d21d1c16dbf66623b8fb5a21452df75e50fb2780
eolv <eolv@vip.qq.com>
2021-10-13 00:46:20 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,8 +1,14 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
  
 def login_mail(username, password):
-	driver = webdriver.Chrome()
+	chrome_options = Options()
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chrome_options.add_argument('--headless')
+	browser = webdriver.Chrome(chrome_options=chrome_options)
+
 	browser.get('https://mail.pku.edu.cn/')
 	browser.find_by_id('username').fill(username)
 	browser.find_by_id('password').fill(password)
