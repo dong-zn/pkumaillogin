a5be9c183c2c1ba903f7b530ac02a2041a90feb4
eolv <eolv@vip.qq.com>
2021-10-13 00:25:03 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,14 +1,17 @@
 import time
-from splinter import Browser
-  
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+ 
 def login_mail(username, password):
-  browser = Browser()
-  browser.visit('https://mail.pku.edu.cn/')
-  browser.find_by_id('username').fill(username)
-  browser.find_by_id('password').fill(password)
-  browser.find_by_id('login_button').click()
-  time.sleep(5)
-  browser.quit()
-  
+	opts = FirefoxOptions()
+	opts.add_argument('–headless')
+	browser = webdriver.Firefox(firefox_options=opts)
+	browser.visit('https://mail.pku.edu.cn/')
+	browser.find_by_id('username').fill(username)
+	browser.find_by_id('password').fill(password)
+	browser.find_by_id('login_button').click()
+	time.sleep(5)
+	browser.quit()
+	
 if __name__ == '__main__':
-  login_mail('2101210560', 'dzn-0419')
+	login_mail('2101210560', 'dzn-0419')
